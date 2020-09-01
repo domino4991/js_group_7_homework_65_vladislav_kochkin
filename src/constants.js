10b70c0c74aa9a1c6e2963846ea52d54214a8a74
Kochkin Vladislav <ltrickfaim@gmail.com>
2020-09-01 22:32:25 +0600

    Названия для навигации

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const PAGES_URL = [
+    {name: 'Poem', id: 'poem'},
+    {name: 'Joke', id: 'joke'},
+    {name: 'Lyrics', id: 'lyrics'},
+    {name: 'About', id: 'about'},
+    {name: 'Contacts', id: 'contacts'}
+];
