84a6e12950b2cef274ab408da6511d3e97d61b95
Kochkin Vladislav <ltrickfaim@gmail.com>
2020-09-02 13:44:17 +0600

    Добавил роуты для страниц и дефолтное оформление

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Layout from "../../components/Layout/Layout";
 import {Route, Switch} from "react-router-dom";
 import MainPage from "../MainPage/MainPage";
+import AdminPage from "../AdminPage/AdminPage";
 
 function App() {
   return (
     <Layout>
       <Switch>
         <Route path="/" exact component={MainPage} />
+        <Route path="/pages/admin" component={AdminPage} />
+        <Route path="/pages/:id" component={MainPage} />
       </Switch>
     </Layout>
   );
