5061c3652d3364e5e3b4c8cf4ea9ee7861a9005c
Kochkin Vladislav <ltrickfaim@gmail.com>
2020-09-02 13:46:30 +0600

    Создал главную страницу сайта

--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './MainPage.css';
+import axiosBase from "../../axiosBase";
+import {Sugar} from "react-preloaders";
 
-const MainPage = () => {
+const MainPage = props => {
+    const [pageContent, setPageContent] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const id = !props.match.params.id ? null : props.match.params.id;
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axiosBase.get(`${id ? `${id}.json` : '/home.json'}`);
+            setPageContent(response.data);
+        };
+        fetchData().finally(() => setLoading(false));
+    }, [id]);
+
     return (
         <section className="Main-page">
-            <h1>Main Page</h1>
+            <Sugar customLoading={loading} />
+            <div className="container">
+                {pageContent !== null ? (
+                    <>
+                        <h1 className="Title">{pageContent.title}</h1>
+                        <div className="Content">{pageContent.content}</div>
+                    </>
+                ) : null}
+            </div>
         </section>
     );
 };
